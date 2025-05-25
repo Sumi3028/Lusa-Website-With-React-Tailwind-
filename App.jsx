0db9f71b442ba318cccab23e78929dc13df1432f
Fatema Akter Sumi <fsumi291@gmail.com>
2025-05-26 05:05:20 +0600

    @Sumi3028 Commit changes

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import Banner from './components/Banner'
+import SectionOne from './components/SectionOne'
+import Offer from './components/Offer'
+import SectionThird from './components/SectionThird'
+import Gallery from './components/Gallery'
+import AboutUs from './components/AboutUs'
+import Email from './components/Email'
+import Blog from './components/Blog'
+import Footer from './components/Footer'
+
+
+
+function App() {
+  return (
+    <>
+     <NavBar/>
+     <Banner/>
+     <SectionOne/>
+     <Offer/>
+     <SectionThird/>
+     <Gallery/>
+     <AboutUs/>
+     <Email/>
+     <Blog/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
